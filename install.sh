4d67d9369ae9681cf2200371170cc2e5da2b35b3
Sam De Meyer <sam.demeyer@robovision.eu>
2023-02-09 16:38:24 +0100

    fix installers

--- install.sh
+++ install.sh
@@ -1,21 +1,7 @@
 #!/usr/bin/env sh
-HOME=`pwd`
 
-# Chamfer Distance
-cd $HOME/extensions/chamfer_dist
-python setup.py install --user
-
-# NOTE: For GRNet 
-
-# Cubic Feature Sampling
-cd $HOME/extensions/cubic_feature_sampling
-python setup.py install --user
-
-# Gridding & Gridding Reverse
-cd $HOME/extensions/gridding
-python setup.py install --user
-
-# Gridding Loss
-cd $HOME/extensions/gridding_loss
-python setup.py install --user
-
+pip install -e extensions/chamfer_dist
+pip install -e extensions/cubic_feature_sampling
+pip install -e extensions/emd
+pip install -e extensions/gridding
+pip install -e extensions/gridding_loss
